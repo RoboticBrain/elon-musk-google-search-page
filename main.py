bba13a25742e1bd30f1b25b0d76b7f0a57786604
Mazhar Ahmed <164842391+RoboticBrain@users.noreply.github.com>
2024-08-09 03:45:42 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# adding shops 
+shops = list
+products = list
+
+def addShops():
+    print("Add some shops")
+    shops = input()
+
+
+def displayShops():
+    for shop in shops:
+        print(shop)
+
+addShops()
+displayShops()
+
+
